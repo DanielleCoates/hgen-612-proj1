8d00382c870904664285283de464bbe1ebbc7ad6
Danielle Coates <coatesdk@vcu.edu>
2025-01-23 14:48:16 -0500

    Increase text size and change text color to black (better readability), change color of dots so the data points can stand out a bit more to audience. Removed unnecessary clutter by getting rid of title, as axis titles are suffice in this case.

--- mtcar_graph.R
+++ mtcar_graph.R
@@ -3,11 +3,13 @@ library(ggplot2)
 mtcars$car_type <- rownames(mtcars)
 
 ggplot(mtcars, aes(x = reorder(car_type, mpg), y = mpg)) +
-  geom_point(size = 2) +
+  geom_point(size = 2, color = 'blue') +
   theme_minimal() +
-  theme(axis.text.y = element_text(size = 7, hjust = 1)) +
-  labs(x = "Car Type", y = "Miles Per Gallon (MPG)", title = "MPG by Car Type") + 
+  theme(axis.text.y = element_text(color = 'black', size = 8, hjust = 1),
+        axis.text.x = element_text(color = 'black', size = 10)) +
+  labs(x = "Car Type", y = "Miles Per Gallon (MPG)") + 
   coord_flip()
+
 
 
 
