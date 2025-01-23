e8356d9707db476e605193fe71c9d02bf350dd3b
Danielle Coates <coatesdk@vcu.edu>
2025-01-23 14:19:20 -0500

    Adjusting horizontal axis for better readability

--- mtcar_graph.R
+++ mtcar_graph.R
@@ -4,7 +4,7 @@ mtcars$car_type <- rownames(mtcars)
 
 ggplot(mtcars, aes(x = car_type, y = mpg)) +
   geom_bar(stat = "identity") +
-  theme(axis.text.x = element_text(angle = 90)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(x = "Car Type", y = "Miles Per Gallon (MPG)", title = "MPG by Car Type")
 
 
