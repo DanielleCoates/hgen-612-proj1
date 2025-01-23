dc2b78d56b3c06adb86c7b72bfeda9b96e88a67c
Danielle Coates <coatesdk@vcu.edu>
2025-01-23 14:56:19 -0500

    Change color of dots to yellow

--- mtcar_graph.R
+++ mtcar_graph.R
@@ -6,7 +6,7 @@ mtcars$car_type <- rownames(mtcars)
 
 #Create a dot plot of car types ordered by miles per gallon
 ggplot(mtcars, aes(x = reorder(car_type, mpg), y = mpg)) +
-  geom_point(size = 2, color = 'blue') +
+  geom_point(size = 2, color = 'yellow') +
   theme_minimal() +
   theme(axis.text.y = element_text(color = 'black', size = 8, hjust = 1),
         axis.text.x = element_text(color = 'black', size = 10)) +
