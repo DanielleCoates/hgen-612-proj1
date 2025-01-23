f7ee0c0ae54fda7049053ed0a43c02c46092268b
Danielle Coates <coatesdk@vcu.edu>
2025-01-23 14:23:55 -0500

    Reorder car type from highest MPG to lowest MPG

--- mtcar_graph.R
+++ mtcar_graph.R
@@ -2,10 +2,11 @@ library(ggplot2)
 
 mtcars$car_type <- rownames(mtcars)
 
-ggplot(mtcars, aes(x = car_type, y = mpg)) +
+ggplot(mtcars, aes(x = reorder(car_type, -mpg), y = mpg)) +
   geom_bar(stat = "identity") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(x = "Car Type", y = "Miles Per Gallon (MPG)", title = "MPG by Car Type")
+
 
 
 
