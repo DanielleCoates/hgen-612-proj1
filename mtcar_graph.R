5d3a9e8995ed478e710a67ae6e795f86300cf0e5
Danielle Coates <coatesdk@vcu.edu>
2025-01-23 14:38:44 -0500

    Flip the graph and make it a dot plot instead of a bar graph

--- mtcar_graph.R
+++ mtcar_graph.R
@@ -2,10 +2,12 @@ library(ggplot2)
 
 mtcars$car_type <- rownames(mtcars)
 
-ggplot(mtcars, aes(x = reorder(car_type, -mpg), y = mpg)) +
-  geom_bar(stat = "identity") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  labs(x = "Car Type", y = "Miles Per Gallon (MPG)", title = "MPG by Car Type")
+ggplot(mtcars, aes(x = reorder(car_type, mpg), y = mpg)) +
+  geom_point(size = 2) +
+  theme_minimal() +
+  theme(axis.text.y = element_text(size = 7, hjust = 1)) +
+  labs(x = "Car Type", y = "Miles Per Gallon (MPG)", title = "MPG by Car Type") + 
+  coord_flip()
 
 
 
