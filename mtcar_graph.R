051359d9603f46054ac3f47300c72ad55a8b04cd
Danielle Coates <coatesdk@vcu.edu>
2025-01-23 14:54:58 -0500

    Adding comments

--- mtcar_graph.R
+++ mtcar_graph.R
@@ -1,7 +1,10 @@
+# Load necessary libraries
 library(ggplot2)
 
+#Extract rownames for independent variable 
 mtcars$car_type <- rownames(mtcars)
 
+#Create a dot plot of car types ordered by miles per gallon
 ggplot(mtcars, aes(x = reorder(car_type, mpg), y = mpg)) +
   geom_point(size = 2, color = 'blue') +
   theme_minimal() +
@@ -9,6 +12,7 @@ ggplot(mtcars, aes(x = reorder(car_type, mpg), y = mpg)) +
         axis.text.x = element_text(color = 'black', size = 10)) +
   labs(x = "Car Type", y = "Miles Per Gallon (MPG)") + 
   coord_flip()
+
 
 
 
